58812f39eb8e4fa417f59f957df187b4bc0bfae0
lutzklai <lutzklai@gmail.com>
2016-04-20 13:09:21 -0500

    comparison module push

--- app/webpages/component-page/component.page.ts
+++ app/webpages/component-page/component.page.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from 'angular2/core';
 import {ComparisonBar} from '../../components/comparison-bar/comparison-bar.component';
+import {ComparisonTile} from '../../components/comparison-tile/comparison-tile.component';
+import {ComparisonModule} from '../../modules/comparison/comparison.module';
 
 @Component({
     selector: 'Component-page',
     templateUrl: './app/webpages/component-page/component.page.html',
-    directives: [ComparisonBar],
+    directives: [ComparisonBar, ComparisonTile, ComparisonModule],
     providers: [],
 })
 
