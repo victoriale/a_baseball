5bc3975d21a32fd535ecd53fb3b55445e3476efa
LarryHPham <larrypham1991@yahoo.com>
2016-04-21 16:01:14 -0500

    fixed empty array in ngfor

--- app/modules/draft-history/draft-history.ts
+++ app/modules/draft-history/draft-history.ts
@@ -17,74 +17,75 @@ interface DraftHistory{
 
 export class DraftHistoryModule{
   moduleTitle:string = "Module Title";
-  testData: Array<Object> = [
-    {
-    dataPoints:[
-      {
-        data:'test',
-        value:'',
-        url:'',
-        icon:'fa fa-share'//test remove when done testing
-      },
-      {
-        data:'[Profile name1]',
-        value:'[Data Value 1]',
-        url:'',
-        icon:''
-      },
-      {
-        data:'[Data Value]: [City], [ST]',
-        value:'[Data Description1]',
-        url:'',
-        icon:'fa fa-map-marker'
-      },
-    ],
-    rank:'1',
-    infoDesc:'Want more info about this [profile type]?',
-    ctaBtn:'',
-    ctaText:'View Profile',
-    ctaUrl:'',
-    footer:{
-      infoDesc:'Want to see everybody involved in this list',
-      btn:'',
-      text:'VIEW THE LIST',
-      url:'',
-    }
-  },
-  {
-    dataPoints:[
-      {
-        data:'',
-        value:'',
-        url:'',
-        icon:''//test remove when done testing
-      },
-      {
-        data:'[Profile name2]',
-        value:'[Data Value 2]',
-        url:'',
-        icon:''
-      },
-      {
-        data:'[Data Value]: [City], [ST]',
-        value:'[Data Description1]',
-        url:'',
-        icon:'fa fa-map-marker'
-      },
-    ],
-    rank:'2',
-    infoDesc:'Want more info about this [profile type]?',
-    ctaBtn:'',
-    ctaText:'View Profile',
-    ctaUrl:'',
-    footer:{
-      infoDesc:'Want to see everybody involved in this list',
-      btn:'',
-      text:'VIEW THE LIST',
-      url:'',
-    }
-  }];
+  testData: Array<Object>
   constructor(){
+    this.testData = [
+      {
+      dataPoints:[
+        {
+          data:'test',
+          value:'',
+          url:'',
+          icon:'fa fa-share'//test remove when done testing
+        },
+        {
+          data:'[Profile name1]',
+          value:'[Data Value 1]',
+          url:'',
+          icon:''
+        },
+        {
+          data:'[Data Value]: [City], [ST]',
+          value:'[Data Description1]',
+          url:'',
+          icon:'fa fa-map-marker'
+        },
+      ],
+      rank:'1',
+      infoDesc:'Want more info about this [profile type]?',
+      ctaBtn:'',
+      ctaText:'View Profile',
+      ctaUrl:'',
+      footer:{
+        infoDesc:'Want to see everybody involved in this list',
+        btn:'',
+        text:'VIEW THE LIST',
+        url:'',
+      }
+    },
+    {
+      dataPoints:[
+        {
+          data:'',
+          value:'',
+          url:'',
+          icon:''//test remove when done testing
+        },
+        {
+          data:'[Profile name2]',
+          value:'[Data Value 2]',
+          url:'',
+          icon:''
+        },
+        {
+          data:'[Data Value]: [City], [ST]',
+          value:'[Data Description1]',
+          url:'',
+          icon:'fa fa-map-marker'
+        },
+      ],
+      rank:'2',
+      infoDesc:'Want more info about this [profile type]?',
+      ctaBtn:'',
+      ctaText:'View Profile',
+      ctaUrl:'',
+      footer:{
+        infoDesc:'Want to see everybody involved in this list',
+        btn:'',
+        text:'VIEW THE LIST',
+        url:'',
+      }
+    }];
     console.log('draft-history test',this.testData);
   }
 }
