626233accd89b415e6a958cd2ab23c5860620804
victoriale <le.victoriavan@gmail.com>
2016-04-21 14:27:36 -0500

    did you know module design

--- app/webpages/team-page/team.page.ts
+++ app/webpages/team-page/team.page.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from 'angular2/core';
 import {LikeUs} from "../../modules/likeus/likeus.module";
+import {DYKModule} from "../../modules/dyk/dyk.module";
 
 @Component({
     selector: 'Team-page',
     templateUrl: './app/webpages/team-page/team.page.html',
-    directives: [LikeUs],
+    directives: [DYKModule, LikeUs],
     providers: [],
 })
 
