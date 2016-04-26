6798cedb451b2f9cb9a2eca11ab462a443d7e11f
Crystal.Prieb <Crystal.Prieb@sntmedia.com>
2016-04-26 17:53:07 -0500

    added About us module-header.component
    * renamed tile component to flip tile
    * removed <a> styles from bootstrap

--- app/modules/aboutus/aboutus.module.ts
+++ app/modules/aboutus/aboutus.module.ts
@@ -1,75 +1,76 @@
-/**
- * Created by Victoria on 2/25/2016.
- */
 import {Component, Input} from 'angular2/core';
-import {ModuleHeader} from "../../components/module-header/module-header.component";
-import {TilesComponent} from "../../components/tiles/tiles.component";
+import {ModuleHeader, ModuleHeaderData} from "../../components/module-header/module-header.component";
+import {FlipTilesComponent, TileData} from "../../components/flip-tiles/flip-tiles.component";
 import {Router,ROUTER_DIRECTIVES} from 'angular2/router';
-import {Injector} from 'angular2/core';
-// import {WebApp} from '../../app-layout/app.layout';
+
 @Component({
-    selector: 'about-us-module',
+    selector: 'about-us',
     templateUrl: './app/modules/aboutus/aboutus.module.html',
-    directives: [ModuleHeader, TilesComponent, ROUTER_DIRECTIVES],
-    providers: [],
-    inputs:['partnerID']
+    directives: [ModuleHeader, FlipTilesComponent, ROUTER_DIRECTIVES],
+    providers: []
 })
-export class AboutUsModule{
-    module_title: string;
-    aboutUsData: any;
-    pageName = "";
-    header = '';
-    logo = '';
-    heading = 'Disclaimer';
-    subText = 'Market data delayed 15 minutes.';
-    mainText = '';
-    providerText = '';
-    currentYear: number;
-    buttonText = 'See The Full Disclaimer';
-    public partnerParam: string;
-    public partnerID: string;
-    constructor(private injector:Injector, private _router: Router){
+export class AboutUsModule {
+    
+    @Input() partnerID: string = null;
+    
+    public homePageLinkName: string = "homerunloyal";
+    public pageName: string;
+    public moduleHeader: ModuleHeaderData;
+    public headerText: string = "Disclaimer";
+    public logoUrl = '/app/public/homeRunLoyalLogo.png';
+    public buttonText = 'See The Full Disclaimer';
+    public aboutUsData: Array<TileData>;
+    
+    constructor(private _router: Router) {
       this._router.root
       .subscribe(
           route => {
-            var curRoute = route;
-            var partnerID = curRoute.split('/');
-            if(partnerID[0] != ''){
-              this.partnerID = partnerID[0];
-              var partnerParam = this.partnerID.replace('-','.');
-            }else{
+            var routeValues = route.split('/');
+            if(routeValues[0] !== '' && routeValues[0] !== undefined && routeValues[0] !== null){
+              //Has Partner
+              this.partnerID = routeValues[0];
+            } else {
               this.partnerID = null;
             }
       })
-      window.scrollTo(0, 0);
     }
-    ngOnInit(){
-      if(this.partnerID === null || this.partnerID == '' || typeof this.partnerID == 'undefined'){
-        this.pageName = "HomeRunLoyal";
-        this.logo = '/app/public/joyfulhome_logo_large.png';
-      } else {
-        this.pageName = "My HouseKit";
-        this.logo = '/app/public/myhousekit_logo.png';
+    
+    ngOnInit() {
+      if(this.partnerID === null) {
+          this.homePageLinkName = "www.homerunloyal.com"
+          this.pageName = "HomeRunLoyal";
+     } else {
+          this.homePageLinkName = "www.myhomerun.com/" + this.partnerID;
+          this.pageName = "My HomeRun";
       }
-      this.header = this.pageName + ' Disclaimer';
-      this.mainText = 'This site is powered by '+this.pageName+'. Ideas and opinions presented on this website are for informational and educational purposes only, and do not reflect the opinions of '+this.pageName+', or any of its affiliates, subsidiaries or partners.';
-      this.currentYear = new Date().getFullYear();
-      this.providerText = this.currentYear+' Data provided by ListHub';
-      this.module_title = 'Learn More About ' + this.pageName;
-      this.aboutUsData = {
-        button_txt: 'Open Page',
-        url1: 'Aboutus-page',
-        icon1: 'fa-info-circle',
-        title1: 'About Us',
-        desc1: 'What is '+ this.pageName +'?',
-        url2: 'Contactus-page',
-        icon2: 'fa-phone',
-        title2: 'Contact Us',
-        desc2: 'Help us help you faster.',
-        url3: 'Disclaimer-page',
-        icon3: 'fa-folder-open-o',
-        title3: 'Disclaimer',
-        desc3: 'Read the full disclaimer.'
-      }
+      
+      this.headerText = this.pageName + ' Disclaimer';
+      this.moduleHeader = {
+        moduleTitle: 'Learn More About ' + this.pageName,
+        hasIcon: false,
+        iconClass: ''
+      };
+      
+      this.aboutUsData = [{
+        buttonText: 'Open Page',
+        routerInfo: ['Aboutus-page'],
+        faIcon: 'fa-info-circle',
+        title: 'About Us',
+        description: 'What is '+ this.pageName +'?',
+      },
+      {
+        buttonText: 'Open Page',
+        routerInfo: ['Contactus-page'],
+        faIcon: 'fa-phone',
+        title: 'Contact Us',
+        description: 'Help us help you faster.',
+      },
+      {
+        buttonText: 'Open Page',
+        routerInfo: ['Disclaimer-page'],
+        faIcon: 'fa-folder-open-o',
+        title: 'Disclaimer',
+        description: 'Read the full disclaimer.'
+      }];
     }
 }
--- app/webpages/component-page/component.page.ts
+++ app/webpages/component-page/component.page.ts
@@ -4,13 +4,14 @@ import {SeasonStatsModule} from '../../modules/season-stats/season-stats.module'
 import {DraftHistoryModule} from '../../modules/draft-history/draft-history';
 import {SchedulesModule} from '../../modules/schedules/schedules';
 import {TeamRosterModule} from '../../modules/team-roster/team-roster.module';
+import {AboutUsModule} from '../../modules/aboutus/aboutus.module';
 import {ShareButtonComponent} from '../../components/share-button/share-button.component';
 import {Search} from '../../components/search/search.component';
 
 @Component({
     selector: 'Component-page',
     templateUrl: './app/webpages/component-page/component.page.html',
-    directives: [ShareButtonComponent, TeamRosterModule, ComparisonModule, DraftHistoryModule, SchedulesModule, SeasonStatsModule, Search],
+    directives: [ShareButtonComponent, TeamRosterModule, ComparisonModule, DraftHistoryModule, SchedulesModule, SeasonStatsModule, Search, AboutUsModule],
     providers: [],
 })
 
