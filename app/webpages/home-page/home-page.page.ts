06e57f98a11b9c082cb3d0c25c0304087651d96c
victoriale <le.victoriavan@gmail.com>
2016-04-26 18:38:23 -0500

    home page

--- app/webpages/home-page/home-page.page.ts
+++ app/webpages/home-page/home-page.page.ts
@@ -1,14 +1,44 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, Input} from 'angular2/core';
 import {FooterComponent} from "../../components/footer/footer.component";
+import {HeaderComponent} from "../../components/header/header.component";
+import {CircleImage} from '../../components/images/circle-image';
+import {ImageData,CircleImageData} from '../../components/images/image-data';
+
+export interface TestImage {
+  imageData: CircleImageData;
+  description: string;
+}
 
 @Component({
     selector: 'home-page',
     templateUrl: './app/webpages/home-page/home-page.page.html',
-    directives: [FooterComponent],
+    directives: [CircleImage, FooterComponent, HeaderComponent],
     inputs: [],
     providers: [],
 })
+
 export class HomePage implements OnInit {
+    public testImages: Array<TestImage>;
+    constructor() {
+      this.getData();
+    }
+    getData(){
+      var sampleImage = "./app/public/placeholder-location.jpg";
+      this.testImages =[
+        {
+          description: "",
+          imageData: {
+            imageClass: "image-100",
+            mainImage: {
+              imageUrl: sampleImage,
+              urlRouteArray: ['Disclaimer-page'],
+              hoverText: "<p>View</p><p>Profile</p>",
+              imageClass: "border-3"
+            }
+          }
+        }];
+
+    }
     ngOnInit() {
     }
 }
