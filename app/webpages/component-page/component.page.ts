aaeada7516d51cf92736c0ccdc1f9e5dab41e634
Crystal.Prieb <Crystal.Prieb@sntmedia.com>
2016-04-27 15:51:57 -0500

    Added Profile Header
    * scrollbar is left unstyled for now.

--- app/webpages/component-page/component.page.ts
+++ app/webpages/component-page/component.page.ts
@@ -7,12 +7,23 @@ import {SchedulesModule} from '../../modules/schedules/schedules';
 import {TeamRosterModule} from '../../modules/team-roster/team-roster.module';
 import {AboutUsModule} from '../../modules/about-us/about-us.module';
 import {ShareButtonComponent} from '../../components/share-button/share-button.component';
+import {ProfileHeaderModule} from '../../modules/profile-header/profile-header.module';
 import {Search} from '../../components/search/search.component';
 
 @Component({
     selector: 'Component-page',
     templateUrl: './app/webpages/component-page/component.page.html',
-    directives: [ShareButtonComponent, TeamRosterModule, ComparisonModule, DraftHistoryModule, SchedulesModule, SeasonStatsModule, Search, AboutUsModule],
+    directives: [
+      ShareButtonComponent, 
+      TeamRosterModule, 
+      ComparisonModule, 
+      DraftHistoryModule, 
+      SchedulesModule, 
+      SeasonStatsModule, 
+      Search,
+      AboutUsModule,
+      ProfileHeaderModule
+    ],
     providers: [],
 })
 
