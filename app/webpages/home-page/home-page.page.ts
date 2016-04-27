6c9b68eb6220b22b0295169b571914c703f6601b
victoriale <le.victoriavan@gmail.com>
2016-04-27 15:56:23 -0500

    add more logic to home page

--- app/webpages/home-page/home-page.page.ts
+++ app/webpages/home-page/home-page.page.ts
@@ -5,9 +5,11 @@ import {CircleImage} from '../../components/images/circle-image';
 import {ImageData,CircleImageData} from '../../components/images/image-data';
 import {Search} from '../../components/search/search.component';
 
-export interface TestImage {
+export interface homePageData {
   imageData: CircleImageData;
-  description: string;
+  location: string;
+  divisionName: string;
+  teamName: string;
 }
 
 @Component({
@@ -19,27 +21,73 @@ export interface TestImage {
 })
 
 export class HomePage implements OnInit {
-    public teamImages: Array<TestImage>;
+    public teamData: Array<homePageData>;
+    public imgHero1: string;
+    public imgIcon1: string;
     public placeholderText: string;
+    public homeHeading1: string;
+    public homeHeading2: string;
+    public homeHeading3: string;
+    public leagueHeading: string;
+    public homeFeatures: string;
     constructor() {
       this.getData();
     }
     getData(){
-      this.placeholderText = "Where do you want to be a fan?";
       var sampleImage = "./app/public/placeholder-location.jpg";
-      this.teamImages =[
+      this.imgHero1 = "/app/public/homePage_hero1.png";
+      this.imgIcon1 = "/app/public/homePage_icon1.png";
+      this.placeholderText = "Where do you want to be a fan?";
+      this.homeHeading1 = "Stay Loyal to Your Favorite MLB Team";
+      this.homeHeading2 = "Find the sports information you need to show your loyalty";
+      this.homeHeading3 = "PICK YOUR FAVORITE <b>MLB TEAM</b>";
+      this.leagueHeading = "<b>AMERICAN LEAGUE</b> TEAMS<b>:</b>";
+      this.homeFeatures = "<b>Features</b> to Note";
+      // this.divisionName = "CENTRAL";
+      this.teamData =[
         {
-          description: "",
+          divisionName: "CENTRAL",
+          location: "CHICAGO",
+          teamName: "White Sox",
           imageData: {
             imageClass: "image-100",
             mainImage: {
               imageUrl: sampleImage,
               urlRouteArray: ['Disclaimer-page'],
-              hoverText: "<i class='fa fa-mail-forward'></i>",
+              hoverText: "<i style='font-size:30px;' class='fa fa-mail-forward'></i>",
               imageClass: "border-3"
             }
           }
-        }];
+        },
+        {
+          divisionName: "EAST",
+          location: "ATLANTA",
+          teamName: "Braves",
+          imageData: {
+            imageClass: "image-100",
+            mainImage: {
+              imageUrl: sampleImage,
+              urlRouteArray: ['Disclaimer-page'],
+              hoverText: "<i style='font-size:30px;' class='fa fa-mail-forward'></i>",
+              imageClass: "border-3"
+            }
+          }
+        },
+        {
+          divisionName: "WEST",
+          location: "ARIZONA",
+          teamName: "Diamondbacks",
+          imageData: {
+            imageClass: "image-100",
+            mainImage: {
+              imageUrl: sampleImage,
+              urlRouteArray: ['Disclaimer-page'],
+              hoverText: "<i style='font-size:30px;' class='fa fa-mail-forward'></i>",
+              imageClass: "border-3"
+            }
+          }
+        }
+      ];
 
     }
     ngOnInit(){}
