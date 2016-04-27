e5002b9adc937a403144add9c5f666ca3088eef7
LarryHPham <larrypham1991@yahoo.com>
2016-04-26 19:48:32 -0500

    changing detailed list item and schedules carousel to use image component

--- app/modules/draft-history/draft-history.ts
+++ app/modules/draft-history/draft-history.ts
@@ -2,6 +2,8 @@ import {Component} from 'angular2/core';
 import {DetailedListItem} from '../../components/detailed-list-item/detailed-list-item';
 import {ModuleFooter} from '../../components/module-footer/module-footer';
 import {ModuleHeader} from '../../components/module-header/module-header.component';
+import {DetailListInput} from '../../components/detailed-list-item/detailed-list-item';
+
 
 @Component({
     selector: 'draft-history',
@@ -13,77 +15,66 @@ import {ModuleHeader} from '../../components/module-header/module-header.compone
 
 export class DraftHistoryModule{
   moduleTitle:string = "Module Title";
-  testData: Array<Object>
+  testData: DetailListInput[];
+  footerData: Object;
   constructor(){
 
     if(typeof this.testData == 'undefined' || this.testData == null){// test data only
-      this.testData = [
+      this.testData =[
         {
         dataPoints:[
           {
             data:'test',
-            value:'',
-            url:'testLink',
+            value:'testvalue',
+            url:['testLink'],
             icon:'fa fa-share'//test remove when done testing
           },
           {
             data:'[Profile name1]',
             value:'[Data Value 1]',
-            url:'',
+            url:[''],
             icon:''
           },
           {
             data:'[Data Value]: [City], [ST]',
             value:'[Data Description1]',
-            url:'testLink',
+            url:['testLink'],
             icon:'fa fa-map-marker'
           },
         ],
-        rank:'1',
-        infoDesc:'Want more info about this [profile type]?',
+        imageConfig: {//interface is found in image-data.ts
+            imageClass: "image-121",
+            mainImage: {
+                imageUrl: "./app/public/placeholder-location.jpg",
+                urlRouteArray: ['Disclaimer-page'],
+                hoverText: "<p>View</p><p>Profile</p>",
+                imageClass: "border-2"
+            },
+            subImages: [
+                {
+                    imageUrl: "./app/public/placeholder-location.jpg",
+                    urlRouteArray: ['Disclaimer-page'],
+                    hoverText: "<i class='fa fa-mail-forward'></i>",
+                    imageClass: "image-40-sub image-round-lower-right"
+                },
+                {
+                    text: "#9",
+                    imageClass: "image-38-rank image-round-upper-left image-round-sub-text"
+                }
+            ],
+        },
+        hasCTA:true,
+        ctaDesc:'Want more info about this [profile type]?',
         ctaBtn:'',
         ctaText:'View Profile',
-        ctaUrl:'',
-        footer:{
-          infoDesc:'Want to see everybody involved in this list',
-          btn:'',
-          text:'VIEW THE LIST',
-          url:'',
-        }
-      },
-      {
-        dataPoints:[
-          {
-            data:'',
-            value:'',
-            url:'',
-            icon:''//test remove when done testing
-          },
-          {
-            data:'[Profile name2]',
-            value:'[Data Value 2]',
-            url:'',
-            icon:''
-          },
-          {
-            data:'[Data Value]: [City], [ST]',
-            value:'[Data Description1]',
-            url:'',
-            icon:'fa fa-map-marker'
-          },
-        ],
-        rank:'2',
-        infoDesc:'Want more info about this [profile type]?',
-        ctaBtn:'',
-        ctaText:'View Profile',
-        ctaUrl:'',
-        footer:{
-          infoDesc:'Want to see everybody involved in this list',
-          btn:'',
-          text:'VIEW THE LIST',
-          url:'',
-        }
+        ctaUrl:[''],
       }];
+      this.footerData = {
+        infoDesc:'Want to see everybody involved in this list',
+        btn:'',
+        text:'VIEW THE LIST',
+        url:'',
+      }
     }
 
     // console.log('draft-history test',this.testData);
