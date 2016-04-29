a1c1db35e9fc7f8d80187d1cfd787f6bd5817822
victoriale <le.victoriavan@gmail.com>
2016-04-29 17:25:49 -0500

    added more styling to footer

--- app/components/footer/footer.component.ts
+++ app/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, OnChanges, Input} from 'angular2/core';
-// import {Router, ROUTER_DIRECTIVES} from "angular2/router";
+import {Router, ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
     selector: 'footer-component',
     templateUrl: './app/components/footer/footer.component.html',
-    // directives: [ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES],
     inputs: [],
     providers: [],
 })
