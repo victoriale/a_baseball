c27ffe4fcda100904e8e4f9d2bebceb67df241e6
victoriale <le.victoriavan@gmail.com>
2016-05-02 14:17:27 -0500

    footer and header styling

--- app/components/footer/footer.component.ts
+++ app/components/footer/footer.component.ts
@@ -11,4 +11,45 @@ import {Router, ROUTER_DIRECTIVES} from "angular2/router";
 export class FooterComponent implements OnInit {
     ngOnInit() {
     }
+
+    teamListings = [
+        { "name": "A"},
+        { "name": "B"},
+        { "name": "C"},
+        { "name": "D"},
+        { "name": "E"},
+        { "name": "F"},
+        { "name": "G"},
+        { "name": "H"},
+        { "name": "I"},
+        { "name": "J"},
+        { "name": "K"},
+        { "name": "L"},
+        { "name": "M"},
+        { "name": "N"},
+        { "name": "O"},
+        { "name": "P"},
+        { "name": "Q"},
+        { "name": "R"},
+        { "name": "S"},
+        { "name": "U"},
+        { "name": "V"},
+        { "name": "W"},
+        { "name": "X"},
+        { "name": "Y"},
+        { "name": "Z"}
+    ];
+    mlbTeams = [
+        { "name": "Arizona Diamondbacks"},
+        { "name": "Atlanta Braves"},
+        { "name": "Baltimore Orioles"},
+        { "name": "Boston Red Sox"},
+        { "name": "Chicago Cubs"},
+        { "name": "Chicago White Sox"},
+        { "name": "Cincinnati Reds"},
+        { "name": "Cleveland Indians"},
+        { "name": "Colorado Rockies"},
+        { "name": "Detroit Tigers"}
+    ];
+
 }
