bb6ea6aa6e39b5d3234a9116386f26f150f98aee
victoriale <le.victoriavan@gmail.com>
2016-05-03 15:25:27 -0500

    faq design completed, started on team roster page, added backtab, title and carousel

--- app/modules/faq/faq.module.ts
+++ app/modules/faq/faq.module.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from 'angular2/core';
-import {ModuleHeader} from '../../components/module-header/module-header.component';
-
+import {ModuleHeader, ModuleHeaderData} from '../../components/module-header/module-header.component';
+export interface faqModuleData{
+  quesiton: string;
+  answer: string;
+}
 @Component({
     selector: 'faq-module',
     templateUrl: './app/modules/faq/faq.module.html',
@@ -9,9 +12,27 @@ import {ModuleHeader} from '../../components/module-header/module-header.compone
 })
 
 export class FAQModule{
-    public moduleTitle: string = 'FAQ - [Profile Name]';
+    public faqData: Object;
+    public headerInfo: ModuleHeaderData = {
+      moduleTitle: "FAQ - [Profile Name]",
+      hasIcon: false,
+      iconClass: ""
+    };
     getData(){
-
+      this.faqData = [
+      {
+        question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dosit amet, consectetur adipisicing elit, sed  eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e?",
+        answer: "Ipsum dolor sit amet, consectetur adipisicing elit, dolor sit amet, consectetur adipisicing elit."
+      },{
+        question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit?",
+        answer: "Ipsum dolor sit amet, consectetur adipisicing elit."
+      },{
+        question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e?",
+        answer: "Ipsum dolor."
+      },{
+        question: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do e?",
+        answer: "Ipsum dolor sit amet, consectetur adipisicing elit."
+      }]
     }
     ngOnInit(){
       this.getData();
--- app/webpages/team-roster/team-roster.page.ts
+++ app/webpages/team-roster/team-roster.page.ts
@@ -7,7 +7,7 @@ import {CircleImage} from '../../components/images/circle-image';
 import {ImageData,CircleImageData} from '../../components/images/image-data';
 import {Tabs} from '../../components/tabs/tabs.component';
 import {Tab} from '../../components/tabs/tab.component';
-import {Carousel} from '../../components/carousels/carousel.component';
+import {SliderCarousel} from '../../components/carousels/slider-carousel/slider-carousel.component';
 import {CustomTable} from '../../components/custom-table/custom-table.component';
 import {TableColumn, TableRow, TableCell} from '../../components/custom-table/table-data.component';
 
@@ -15,14 +15,17 @@ import {TableColumn, TableRow, TableCell} from '../../components/custom-table/ta
     selector: 'Teamroster-page',
     templateUrl: './app/webpages/team-roster/team-roster.page.html',
 
-    directives: [BackTabComponent, TitleComponent, CircleImage, Tabs, Tab, Carousel, CustomTable, ROUTER_DIRECTIVES],
+    directives: [BackTabComponent, TitleComponent, CircleImage, Tabs, Tab, SliderCarousel, CustomTable, ROUTER_DIRECTIVES],
     providers: [],
 })
 
 export class TeamrosterPage implements OnInit{
   titleData: TitleInputData;
+  public carouselData: Array<any>;
+
   constructor() {
     this.getData();
+    this.carData();
   }
   getData(){
     this.titleData = {
@@ -35,6 +38,70 @@ export class TeamrosterPage implements OnInit{
         hasHover: false
     };
 
+  }
+  carData(){
+    //Carousel Data Below is an array of dummy carouselData that should be replaced with real data
+    var sampleImage = "./app/public/placeholder-location.jpg";
+    var carouselData =[
+      {
+        imageConfig: {
+          imageClass: "image-150",
+          mainImage: {
+            imageUrl: sampleImage,
+            urlRouteArray: ['Disclaimer-page'],
+            hoverText: "<p>View</p>Profile",
+            imageClass: "border-large"
+          },
+          subImages: [
+            {
+              imageUrl: sampleImage,
+              urlRouteArray: ['Disclaimer-page'],
+              hoverText: "<i class='fa fa-mail-forward'></i>",
+              imageClass: "image-50-sub image-round-lower-right"
+            },
+            {
+              text: "#1",
+              imageClass: "image-38-rank image-round-upper-left image-round-sub-text"
+            }
+          ],
+        },
+        description: [
+          "<p>Line4</p>",
+          "<p>Line7</p>",
+          "<p>Line8</p>",
+          "<p>Line3</p>",
+        ],
+      },
+      {
+        imageConfig: {
+          imageClass: "image-150",
+          mainImage: {
+            imageUrl: sampleImage,
+            urlRouteArray: ['Disclaimer-page'],
+            hoverText: "<p>View</p>Profile",
+            imageClass: "border-large"
+          },
+          subImages: [
+            {
+              imageUrl: sampleImage,
+              urlRouteArray: ['Disclaimer-page'],
+              hoverText: "<i class='fa fa-mail-forward'></i>",
+              imageClass: "image-50-sub image-round-lower-right"
+            },
+            {
+              text: "#1",
+              imageClass: "image-38-rank image-round-upper-left image-round-sub-text"
+            }
+          ],
+        },
+        description: [
+          "<p>Line1</p>",
+          "<p>Line2</p>",
+          "<p>Line3</p>",
+          "<p>Line4</p>",
+        ],
+      },]
+    this.carouselData = carouselData;
   }
   ngOnInit(){}
 }
