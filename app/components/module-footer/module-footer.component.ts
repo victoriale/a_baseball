f0bc4d4a3a976c11959dc3b8af1025e7c3222f9f
LarryHPham <larrypham1991@yahoo.com>
2016-05-03 12:58:06 -0500

    update to an existing link for mod footer

--- app/components/module-footer/module-footer.component.ts
+++ app/components/module-footer/module-footer.component.ts
@@ -21,7 +21,7 @@ export class ModuleFooter implements OnInit{
         this.footerData = {
           infoDesc:'Want to see everybody involved in this list',
           text:'VIEW THE LIST',
-          url:['profile-page'],
+          url:['Disclaimer-page'],
         }
       }
     }
