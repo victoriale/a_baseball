4144b0ff6b1aaa4b39f90853439647f701831f9b
Crystal.Prieb <Crystal.Prieb@sntmedia.com>
2016-05-05 10:32:53 -0500

    Integrated the backend API with the About Us page
    * also cleaned up the page's HTML

--- app/services/about-us.service.ts
+++ app/services/about-us.service.ts
@@ -2,37 +2,83 @@ import {Injectable} from 'angular2/core';
 import {Observable} from 'rxjs/Rx';
 import {Http} from 'angular2/http';
 
+import {TitleInputData} from "../components/title/title.component";
+import {GlobalFunctions} from "../global/global-functions";
+
+export interface AuBlockData {
+  iconUrl:string;
+  titleText:string;
+  dataText:string;
+}
+
 export interface AboutUsInterface {
     teamProfilesCount: number;
     divisionsCount: number;
     playerProfilesCount: number;
-    worldChampionName: string;
-    worldChampionYear: string;
-    worldChampionImageUrl: string;
+    worldChampName: string;
+    worldChampYear: string;
+    worldChampImageUrl: string;
+    lastUpdatedDate: Date; //TODO-CJP: Needed in API
+}
+
+export interface AboutUsModel {
+    blocks: Array<AuBlockData>;
+    headerTitle: string;
+    titleData: TitleInputData;
 }
 
 @Injectable()
 export class AboutUsService {
-  private _apiUrl: string = 'http://api2.joyfulhome.com';
-  private _defaultData: AboutUsInterface = {
-    teamProfilesCount: 96,
-    divisionsCount: 15,
-    playerProfilesCount: 1512,
-    worldChampionName: "Jayhawks",
-    worldChampionYear: "2016",
-    worldChampionImageUrl: "Jayhawks.png",
-  }
+  private _apiUrl: string = 'http://dev-homerunloyal-api.synapsys.us/landingPage';
+  
+  constructor(public http: Http, private _globalFunctions: GlobalFunctions){}
 
-  constructor(public http: Http){}
-
-  getAboutUsData(): Observable<AboutUsInterface> {
+  getData(partnerID: string): Observable<AboutUsModel> {
     let url = this._apiUrl + '/aboutUs';
     return this.http.get(url)
         .map(res => res.json())
-        .map(data => data.data);
+        .map(data => this.formatData(data.data, partnerID));
   }
   
-  getAboutUsDefaultData(): Observable<AboutUsInterface> {
-    return Observable.of(this._defaultData);
+  private formatData(data: AboutUsInterface, partnerID: string): AboutUsModel {
+    let pageName = (partnerID === null)
+            ? "Home Run Loyal" 
+            : "My Home Run Loyal";
+    let lastUpdatedDate = data.lastUpdatedDate !== undefined ? data.lastUpdatedDate : new Date(); //TODO-CJP: update when included in API
+    let model: AboutUsModel = {
+      headerTitle: "What is " + pageName + "?",
+      titleData: {
+          imageURL : '/app/public/mainLogo.png',
+          text1: 'Last Updated: ' + this._globalFunctions.formatUpdatedDate(lastUpdatedDate),
+          text2: 'United States',
+          text3: "Want to learn more about " + pageName + "?",
+          text4: '',
+          icon: 'fa fa-map-marker'
+      },
+      blocks: [
+        {
+          iconUrl: '/app/public/aboutUs_logo1.png',
+          titleText: 'MLB Team Profiles',
+          dataText: this._globalFunctions.commaSeparateNumber(data.teamProfilesCount)
+        },
+        {
+          iconUrl: '/app/public/aboutUs_logo2.png',
+          titleText: 'MLB Divisions',
+          dataText: this._globalFunctions.commaSeparateNumber(data.divisionsCount)
+        },
+        {
+          iconUrl: '/app/public/aboutUs_logo3.png',
+          titleText: 'MLB Player Profiles',
+          dataText: this._globalFunctions.commaSeparateNumber(data.playerProfilesCount)
+        },
+        {
+          iconUrl: data.worldChampImageUrl,
+          titleText: data.worldChampYear + ' World Series Champions',
+          dataText: data.worldChampName
+        }
+      ]
+    };
+    
+    return model;
   }
 }
