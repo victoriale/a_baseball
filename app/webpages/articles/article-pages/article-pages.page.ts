2ae6528fbb7883e0ed781248d07f7b50d646fa10
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-05-09 10:54:28 -0500

    Updated less files, removed unneeded pages and added one master page with routing for all article pages.

--- app/webpages/articles/article-pages/article-pages.page.ts
+++ app/webpages/articles/article-pages/article-pages.page.ts
@@ -10,55 +10,61 @@ import {ArticleImages} from "../../../components/articles/carousel/carousel.comp
 import {ArticleContentComponent} from "../../../components/articles/article-content/article-content.component";
 import {DisqusComponent} from "../../../components/articles/disqus/disqus.component";
 import {ArticleDataService} from "../../../global/global-article-page-service";
-import {GlobalFunctions} from '../../../global/global-functions';
+import {GlobalFunctions} from "../../../global/global-functions";
 
 declare var jQuery:any;
 
 @Component({
-    selector: 'Articles-history-page',
-    templateUrl: './app/webpages/articles/about/about.page.html',
+    selector: 'article-pages',
+    templateUrl: './app/webpages/articles/article-pages/article-pages.page.html',
     directives: [WidgetModule, ROUTER_DIRECTIVES, ArticleImages, ShareLinksComponent, ArticleContentComponent, RecommendationsComponent, TrendingComponent, DisqusComponent],
     providers: [],
 })
 
-export class ArticlePageAbout implements OnInit {
+export class ArticlePages implements OnInit {
     articleData:ArticleData;
     randomHeadlines:any;
     images:any;
+    eventID:string;
+    eventType:string;
     title:string;
     date:string;
     content:string;
     url:string;
-    eventID:string;
     comment:string;
-    singleLogo:boolean = false;
     pageIndex:string;
+    doubleLogo:boolean = false;
+    articleType:string;
+    articleSubType:string;
     public partnerParam:string;
     public partnerID:string;
 
     constructor(private _params:RouteParams, private _articleDataService:ArticleDataService, private _globalFunctions:GlobalFunctions) {
         window.scrollTo(0, 0);
         this.eventID = _params.get('eventID');
+        this.eventType = _params.get('eventType');
         this.getArticles();
     }
 
     getArticles() {
-        this._articleDataService.getAboutTheTeamsData(this.eventID)
+        this.getArticleType();
+        this._articleDataService.getArticleData(this.eventID, this.eventType)
             .subscribe(
                 ArticleData => {
-                    this.articleData = ArticleData['about-the-teams'];
-                    this.title = ArticleData['about-the-teams'].displayHeadline;
-                    this.date = ArticleData['about-the-teams'].dateline;
-                    this.comment = ArticleData['about-the-teams'].commentHeader;
+                    var pageIndex = Object.keys(ArticleData)[0];
+                    this.articleData = ArticleData[pageIndex];
+                    this.title = ArticleData[pageIndex].displayHeadline;
+                    this.date = ArticleData[pageIndex].dateline;
+                    this.comment = ArticleData[pageIndex].commentHeader;
                     this.url = window.location.href;
-                    this.singleLogo = true;
-                    ArticlePageAbout.setMetaTag(this.articleData.metaHeadline);
+                    this.doubleLogo = true;
+                    ArticlePages.setMetaTag(this.articleData.metaHeadline);
                 }
             );
         this._articleDataService.getRecommendationsData(this.eventID)
             .subscribe(
                 HeadlineData => {
-                    this.pageIndex = "about-the-teams";
+                    this.pageIndex = this.eventType;
                     this.eventID = HeadlineData.event;
                     this.getRandomArticles(HeadlineData, this.pageIndex, this.eventID);
                 }
@@ -67,7 +73,6 @@ export class ArticlePageAbout implements OnInit {
 
     getRandomArticles(recommendations, pageIndex, eventID) {
         var recommendArr = [];
-        var self = this;
         jQuery.map(recommendations.leftColumn, function (val, index) {
             if (pageIndex != index) {
                 switch (index) {
@@ -80,7 +85,7 @@ export class ArticlePageAbout implements OnInit {
                     case'injuries-away':
                     case'upcoming-game':
                         val['title'] = val.displayHeadline;
-                        val['pageIndex'] = self._globalFunctions.toTitleCase(index.toString());
+                        val['eventType'] = index;
                         val['eventID'] = eventID;
                         //val['photos'] = val[0].photos.url;
                         recommendArr.push(val);
@@ -100,8 +105,15 @@ export class ArticlePageAbout implements OnInit {
                     case'left-field-player-comparison':
                     case'center-field-player-comparison':
                     case'right-field-player-comparison':
+                    case'outfield-most-putouts':
+                    case'outfielder-most-hits':
+                    case'outfield-most-home-runs':
+                    case'infield-most-hits':
+                    case'infield-most-home-runs':
+                    case'infield-best-batting-average':
+                    case'infield-most-putouts':
                         val['title'] = val.displayHeadline;
-                        val['pageIndex'] = self._globalFunctions.toTitleCase(index.toString());
+                        val['eventType'] = index;
                         val['eventID'] = eventID;
                         recommendArr.push(val);
                         break;
@@ -112,7 +124,56 @@ export class ArticlePageAbout implements OnInit {
             return 0.5 - Math.random()
         });
         this.randomHeadlines = recommendArr;
-        this.images = recommendations['images'].home;
+        this.images = recommendations['home'].images;
+    }
+
+    getArticleType() {
+        switch (this.eventType) {
+            case'about-the-teams':
+                this.articleType = 'teamRecord';
+                this.articleSubType = 'about';
+                break;
+            case'historical-team-statistics':
+                this.articleType = 'teamRecord';
+                this.articleSubType = 'history';
+                break;
+            case'last-matchup':
+                this.articleType = 'teamRecord';
+                this.articleSubType = 'last';
+                break;
+            case'starting-lineup-home':
+            case'starting-lineup-away':
+            case'injuries-home':
+            case'injuries-away':
+                this.articleType = 'playerRoster';
+                break;
+            case'pitcher-player-comparison':
+            case'catcher-player-comparison':
+            case'first-base-player-comparison':
+            case'second-base-player-comparison':
+            case'third-base-player-comparison':
+            case'shortstop-player-comparison':
+            case'left-field-player-comparison':
+            case'center-field-player-comparison':
+            case'right-field-player-comparison':
+            case'outfield-most-putouts':
+            case'outfielder-most-hits':
+            case'outfield-most-home-runs':
+            case'infield-most-hits':
+            case'infield-most-home-runs':
+            case'infield-best-batting-average':
+            case'infield-most-putouts':
+                this.articleType = 'playerComparison';
+                break;
+            case'pregame-report':
+            case'third-inning-report':
+            case'fifth-inning-report':
+            case'Seventh-inning-stretch-report':
+            case'postgame-report':
+                this.articleType = 'gameReport';
+                break;
+        }
+        return this.articleType;
     }
 
     static setMetaTag(metaData) {
