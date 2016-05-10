79173cf23345ab08ab5a68649fc58448664c8039
LarryHPham <larrypham1991@yahoo.com>
2016-05-10 10:26:16 -0500

    added design page

---
+++ app/webpages/design-page/design.page.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from 'angular2/core';
+
+
+@Component({
+    selector: 'Design-page',
+    templateUrl: './app/webpages/design-page/design.page.html',
+    directives: [],
+    providers: []
+})
+
+export class DesignPage implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
