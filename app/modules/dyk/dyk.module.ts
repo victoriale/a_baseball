8e87d9e2ee8ba0a92c7b5070497543252f776541
victoriale <victoria@sntmedia.com>
2016-05-16 16:17:08 -0500

    dyk service

--- app/modules/dyk/dyk.module.ts
+++ app/modules/dyk/dyk.module.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, Input} from 'angular2/core';
 import {ModuleHeader} from '../../components/module-header/module-header.component';
+import {DYKService} from '../../services/dyk.service';
 
 export interface dykData{
   info: string;
@@ -9,13 +10,21 @@ export interface dykData{
     selector: 'dyk-module',
     templateUrl: './app/modules/dyk/dyk.module.html',
     directives: [ModuleHeader],
-    providers: []
+    providers: [DYKService]
 })
 
 export class DYKModule{
     public moduleTitle: string = 'Did You Know - [Profile Name]';
     public dykInfo: Object;
-
+    constructor(private _dykService: DYKService) {
+      this.getData();
+    }
+    getData(){
+      this._dykService.getDYKService()
+        .subscribe(data => {
+          console.log(data);
+        })
+    }
     ngOnInit(){
       this.dykInfo = [{
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa."
---
+++ app/services/dyk.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
+import {Http, Headers} from 'angular2/http';
+import {GlobalSettings} from '../global/global-settings';
+
+@Injectable()
+export class DYKService {
+  private _apiUrl: string = GlobalSettings.getApiUrl();
+  constructor(public http: Http){}
+  setToken(){
+    var headers = new Headers();
+    return headers;
+  }
+  getDYKService(){
+    var headers = this.setToken();
+    var fullUrl = this._apiUrl + "/league/didYouKnow";
+    return this.http.get(fullUrl, {
+      headers: headers
+    })
+    .map(
+      res => res.json()
+    )
+    .map(
+      data => {
+        console.log(data);
+        return {
+            data
+        };
+      }
+    )
+  }//getDYKService ends
+}//DYKService ENDS
