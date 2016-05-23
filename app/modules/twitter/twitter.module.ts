5a7140598af150ab113eee0d0132a73d4375b41e
victoriale <victoria@sntmedia.com>
2016-05-22 23:31:47 -0500

    added default mlb twitter for test

--- app/modules/twitter/twitter.module.ts
+++ app/modules/twitter/twitter.module.ts
@@ -9,24 +9,33 @@ import {ModuleHeader, ModuleHeaderData} from '../../components/module-header/mod
 })
 
 export class TwitterModule implements OnInit, OnChanges {
-  
+
   @Input() profileName: string;
-  
+
   public headerInfo: ModuleHeaderData = {
     moduleTitle: "Twitter Feed - [Profile Name]",
     hasIcon: false,
     iconClass: ""
   };
-    
+
   ngOnChanges() {
     let profileName = this.profileName ? this.profileName : "[Profile Name]";
     this.headerInfo.moduleTitle = "Twitter Feed - " + profileName;
   }
-  
+
   getData() {
   }
-  
+
   ngOnInit() {
     this.getData();
+    var script:any = document.createElement("script");
+    script.innerHTML = !function(d,s,id){
+        var js, fjs = d.getElementsByTagName(s)[0], p=/^http:/.test(d.location)?'http':'https';
+        if(!d.getElementById(id)){
+          js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document,"script","twitter-wjs");
+    document.body.appendChild(script);
   }
 }
