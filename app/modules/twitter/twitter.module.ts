72bd32d621eab583d58699713da0729e4a893629
victoriale <victoria@sntmedia.com>
2016-05-23 12:59:25 -0500

    set up twitter service

--- app/modules/twitter/twitter.module.ts
+++ app/modules/twitter/twitter.module.ts
@@ -1,16 +1,20 @@
 import {Component, OnInit, OnChanges, Input} from 'angular2/core';
 import {ModuleHeader, ModuleHeaderData} from '../../components/module-header/module-header.component';
+export interface twitterModuleData{
+  twitterUrl: string;
+  twitterApi: string;
+  twitterUserName: string;
+}
 
 @Component({
     selector: 'twitter-module',
     templateUrl: './app/modules/twitter/twitter.module.html',
     directives: [ModuleHeader],
-    providers: []
 })
 
 export class TwitterModule implements OnInit, OnChanges {
-
   @Input() profileName: string;
+  @Input() twitterData: twitterModuleData;
 
   public headerInfo: ModuleHeaderData = {
     moduleTitle: "Twitter Feed - [Profile Name]",
@@ -23,18 +27,14 @@ export class TwitterModule implements OnInit, OnChanges {
     this.headerInfo.moduleTitle = "Twitter Feed - " + profileName;
   }
 
-  getData() {
-  }
-
   ngOnInit() {
-    this.getData();
     var script:any = document.createElement("script");
     script.innerHTML = !function(d,s,id){
-        var js, fjs = d.getElementsByTagName(s)[0], p=/^http:/.test(d.location)?'http':'https';
-        if(!d.getElementById(id)){
-          js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+      var js, fjs = d.getElementsByTagName(s)[0], p=/^http:/.test(d.location)?'http':'https';
+      if(!d.getElementById(id)){
+        js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
         fjs.parentNode.insertBefore(js,fjs);
-        }
+      }
     }(document,"script","twitter-wjs");
     document.body.appendChild(script);
   }
