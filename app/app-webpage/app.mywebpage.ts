e1ae05b3f540331946553447b352a5eeeab836e1
cjprieb <cjprieb@gmail.com>
2016-06-03 13:24:40 -0500

    Cleaned up imports

--- app/app-webpage/app.mywebpage.ts
+++ app/app-webpage/app.mywebpage.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, Injector} from '@angular/core';
-import {Router, ROUTER_DIRECTIVES, } from "@angular/router-deprecated";
-import {RouteParams, RouteData, RouteConfig, RouterOutlet, RouterLink} from '@angular/router-deprecated';
+import {Router, ROUTER_DIRECTIVES, RouteParams, RouteData, RouteConfig, RouterOutlet, RouterLink} from "@angular/router-deprecated";
 // import {ListPage} from "../webpages/list-page/list.page";
 // import {ListOfListsPage} from "../webpages/list-of-lists-page/list-of-lists.page";
 import {AboutUsPage} from "../webpages/about-us-page/about-us.page";
