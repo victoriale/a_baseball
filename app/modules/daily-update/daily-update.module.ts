3047261f46cd7625f72fbd41c487344520c32775
Jonathan Van Winkle <jonathan.vanwinkle@sntmedia.com>
2016-06-09 15:01:18 -0500

    further design build out of dailyUpdate module

--- app/modules/daily-update/daily-update.module.ts
+++ app/modules/daily-update/daily-update.module.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from 'angular2/core';
 import {ModuleHeader, ModuleHeaderData} from '../../components/module-header/module-header.component';
+import {CircleImageData} from "../../components/images/image-data";
+import {CircleImage} from "../../components/images/circle-image";
+import {GlobalSettings} from "../../global/global-settings";
 
 @Component({
     selector: 'daily-update-module',
     templateUrl: './app/modules/daily-update/daily-update.module.html',
-    directives: [ModuleHeader],
+    directives: [ModuleHeader, CircleImage],
     providers: []
 })
 
@@ -20,6 +23,20 @@ export class DailyUpdateModule{
       iconClass: null
     };
 
-    constructor(){ }
+    sampleImage = "./app/public/no-image.png";
+
+  public imageConfig: CircleImageData;
+
+
+    constructor(){
+    this.imageConfig = {
+        imageClass: "image-121",
+        mainImage: {
+          imageClass: "border-2",
+          imageUrl: GlobalSettings.getSiteLogoUrl(),
+          placeholderImageUrl: GlobalSettings.getSiteLogoUrl()
+        }
+      };
+    }
 
 }
