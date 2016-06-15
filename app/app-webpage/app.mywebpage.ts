93eab5b7c502d9e6ae59c0709f0876307a083246
Crystal.Prieb <Crystal.Prieb@sntmedia.com>
2016-06-15 10:44:48 -0500

    Added some routes to the partner site

--- app/app-webpage/app.mywebpage.ts
+++ app/app-webpage/app.mywebpage.ts
@@ -1,6 +1,11 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, RouterOutlet, ROUTER_DIRECTIVES} from 'angular2/router';
 
+import {TeamPage} from "../webpages/team-page/team.page";
+import {MLBPage} from "../webpages/mlb-page/mlb.page";
+import {PlayerPage} from "../webpages/player-page/player.page";
+import {HomePage} from "../webpages/home-page/home-page.page";
+
 import {AboutUsPage} from "../webpages/about-us-page/about-us.page";
 import {ComponentPage} from "../webpages/component-page/component.page";
 
@@ -15,12 +20,29 @@ import {PartnerHeader} from "../global/global-service";
 })
 
 @RouteConfig([
+    //Home Page
     {
         path: '/',
-        name: 'About-us-page',
-        component: AboutUsPage,
+        name: 'Home-page',
+        component: HomePage,
         useAsDefault: true
-    }
+    },
+    //Profile Pages
+    {
+        path: '/profile/mlb',
+        name: 'MLB-page',
+        component: MLBPage,
+    },
+    {
+        path: '/profile/team/:teamName/:teamId',
+        name: 'Team-page',
+        component: TeamPage,
+    },
+    {
+        path: '/profile/player/:teamName/:fullName/:playerId',
+        name: 'Player-page',
+        component: PlayerPage,
+    },
 ])
 
 export class MyAppComponent {}
