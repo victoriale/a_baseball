831df7f42f1f207c8370c3fc386a01f10cc61378
Crystal.Prieb <Crystal.Prieb@sntmedia.com>
2016-06-29 09:18:14 -0500

    Added loading icon to draft history

--- app/webpages/draft-history-page/draft-history.page.ts
+++ app/webpages/draft-history-page/draft-history.page.ts
@@ -3,82 +3,64 @@ import {RouteParams} from 'angular2/router';
 import {Title} from 'angular2/platform/browser';
 
 import {GlobalSettings} from "../../global/global-settings";
-import {DetailedListItem, DetailListInput} from '../../components/detailed-list-item/detailed-list-item.component';
-import {SliderCarousel, SliderCarouselInput} from '../../components/carousels/slider-carousel/slider-carousel.component';
-import {Tabs} from '../../components/tabs/tabs.component';
-import {Tab} from '../../components/tabs/tab.component';
 import {TitleComponent, TitleInputData} from '../../components/title/title.component';
 import {BackTabComponent} from '../../components/backtab/backtab.component';
-import {DraftHistoryService} from '../../services/draft-history.service';
-import {ListPageService} from '../../services/list-page.service';
-import {NoDataBox} from '../../components/error/data-box/data-box.component';
-import {ProfileHeaderService} from '../../services/profile-header.service';
 import {LoadingComponent} from "../../components/loading/loading.component";
 import {ErrorComponent} from "../../components/error/error.component";
 import {SidekickWrapper} from "../../components/sidekick-wrapper/sidekick-wrapper.component";
+import {DraftHistoryComponent} from "../../components/draft-history/draft-history.component";
+import {IProfileData, ProfileHeaderService} from '../../services/profile-header.service';
 
 @Component({
     selector: 'draft-history-page',
     templateUrl: './app/webpages/draft-history-page/draft-history.page.html',
-    directives: [SidekickWrapper, ErrorComponent, LoadingComponent, NoDataBox, BackTabComponent, TitleComponent, Tab, Tabs, SliderCarousel, DetailedListItem],
-    providers: [DraftHistoryService, ProfileHeaderService, Title]
+    directives: [SidekickWrapper, ErrorComponent, LoadingComponent, DraftHistoryComponent, BackTabComponent, TitleComponent],
+    providers: [ProfileHeaderService, Title]
 })
 
 export class DraftHistoryPage implements OnInit{
   whatProfile:string = "Draft History";
+
   profileHeaderData: TitleInputData;
-  errorData: any;
-  dataArray: any;//array of data for detailed list
-  detailedDataArray: Array<DetailListInput>; //variable that is just a list of the detailed DataArray
-  carouselDataArray: Array<SliderCarouselInput>;
-  // carouselFooter: any = {
-  //   ctaBoxClass: "list-footer",
-  //   ctaBtnClass:"list-footer-btn",
-  //   hasIcon: true,
-  // };
-  profileName: string;
-  teamId: number;
+
+  profileData: IProfileData;
+
   isError: boolean = false;
-  constructor(private draftService:DraftHistoryService, 
-              private profHeadService:ProfileHeaderService, 
+
+  constructor(private _profileService:ProfileHeaderService, 
               private params: RouteParams, 
               private _title: Title) {
-    _title.setTitle(GlobalSettings.getPageTitle("Draft History"));
-    if ( this.params.params['teamId'] ) {
-      this.teamId = Number(this.params.params['teamId']);
-    }
+    _title.setTitle(GlobalSettings.getPageTitle(this.whatProfile));
   }
 
-  getProfileInfo() {
-    //MLB starts and ends in same year so can use current year logic to grab all current season and back 4 years for tabs
-    var currentTab = new Date().getFullYear();
+  ngOnInit() {
+    let teamId = null;
+    if ( this.params.get('teamId') != null ) {
+      teamId = Number(this.params.get('teamId'));
+    }
 
-    if ( this.teamId ) {
-      this.profHeadService.getTeamProfile(this.teamId)
+    if ( teamId ) {
+      this._profileService.getTeamProfile(teamId)
       .subscribe(
           data => {
             this._title.setTitle(GlobalSettings.getPageTitle("Draft History", data.teamName));
-            var stats = data.headerData.stats;
-            var pageNameForTitle = stats.teamName + " " + stats.seasonId + " - " + this.whatProfile;
-            this.profileHeaderData = this.profHeadService.convertTeamPageHeader(data, pageNameForTitle);
-            this.profileName = stats.teamName;
-            this.getDraftPage(currentTab);
+            var pageNameForTitle = data.profileName + " " + data.headerData.stats.seasonId + " - " + this.whatProfile;
+            this.profileHeaderData = this._profileService.convertTeamPageHeader(data, pageNameForTitle);
+            this.profileData = data;
           },
           err => {
             this.isError= true;
               console.log('Error: draftData Profile Header API: ', err);
-              // this.isError = true;
           }
       );
     }
     else {
-      this.profHeadService.getMLBProfile()
+      this._profileService.getMLBProfile()
       .subscribe(
           data => {
-            this._title.setTitle(GlobalSettings.getPageTitle("Draft History", data.profileName1));
-            this.profileHeaderData = this.profHeadService.convertMLBHeader(data, this.whatProfile);
-            this.profileName = "MLB";
-            this.getDraftPage(currentTab);
+            this._title.setTitle(GlobalSettings.getPageTitle("Draft History", data.headerData.profileNameShort));
+            this.profileHeaderData = this._profileService.convertMLBHeader(data.headerData, this.whatProfile);
+            this.profileData = data;
           },
           err => {
             this.isError= true;
@@ -87,44 +69,4 @@ export class DraftHistoryPage implements OnInit{
       );
     }
   }
-
-  getDraftPage(date) {
-    this.errorData = {
-      data: "Sorry, " + (this.teamId ? "the " + this.profileName + " do" : "MLB does") + " not currently have any data for the " + date + " " + this.whatProfile,
-      icon: "fa fa-remove"
-    }
-    this.draftService.getDraftHistoryService(date, this.teamId, this.errorData.data, 'page')
-        .subscribe(
-            draftData => {
-              if(typeof this.dataArray == 'undefined'){//makes sure it only runs once
-                this.dataArray = draftData.tabArray;
-              }
-              if(draftData.listData.length == 0){//makes sure it only runs once
-                this.detailedDataArray = null;
-              }else{
-                this.detailedDataArray = draftData.listData;
-              }
-              this.carouselDataArray = draftData.carData;
-            },
-            err => {
-              this.isError= true;
-                console.log('Error: draftData API: ', err);
-            }
-        );
-  }
-
-  ngOnInit(){
-    this.getProfileInfo();
-  }
-
-  selectedTab(event){
-    var firstTab = 'Current Season';
-
-    if(event == firstTab){
-      event = new Date().getFullYear();
-    }
-    this.getDraftPage(event);
-  }
-
-
 }
