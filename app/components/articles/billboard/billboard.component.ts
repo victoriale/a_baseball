285215c5b1bd2b68b8abd53054e4e0d6dd3c98c3
LarryHPham <larrypham1991@yahoo.com>
2016-07-01 14:13:20 -0500

    fixed article pages with rc2 update

--- app/components/articles/billboard/billboard.component.ts
+++ app/components/articles/billboard/billboard.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {DomSanitizationService, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
     selector: 'billboard-component',
@@ -6,5 +7,16 @@ import {Component, Input} from '@angular/core';
     inputs: ["teamId"],
 })
 
-export class BillboardComponent {
+export class BillboardComponent implements OnInit{
+  dangerousBillBoardUrl: string;
+
+  safeBillBoardUrl: SafeResourceUrl;
+
+  constructor(private _sanitizer: DomSanitizationService) {
+  }
+
+  ngOnInit(){
+    this.dangerousBillBoardUrl = "http://devapi.synapsys.us/widgets/sports/ai_billboard.html?%7B%22team%22%3A%22"+this.teamId+"%22%2C%22remn%22%3A%22true%22%7D"
+    this.safeBillBoardUrl = this._sanitizer.bypassSecurityTrustResourceUrl(this.dangerousBillBoardUrl);
+  }
 }
