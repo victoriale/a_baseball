9039d1942f7523108d5e49df16601adcb1a5b16f
Alex Varenhorst <alexvarenhorst@gmail.com>
2016-07-12 15:10:51 -0500

    featued tile stack module. Is responsive

--- app/webpages/deep-dive-page/deep-dive.page.ts
+++ app/webpages/deep-dive-page/deep-dive.page.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {TileStackModule} from '../../modules/tile-stack/tile-stack.module';
+
 
 @Component({
     selector: 'deep-dive-page',
     templateUrl: './app/webpages/deep-dive-page/deep-dive.page.html',
-    directives: [],
+    directives: [TileStackModule],
     providers: [],
 })
 
