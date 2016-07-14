615b3cc0987d630bbc0c1492412c7d749d90e2ab
LarryHPham <larrypham1991@yahoo.com>
2016-07-13 22:39:18 -0500

    schedule box

--- app/modules/side-scroll-schedules/side-scroll-schedules.module.ts
+++ app/modules/side-scroll-schedules/side-scroll-schedules.module.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {ScheduleBox} from '../../components/schedule-box/schedule-box.component'
 
 @Component({
     selector: 'side-scroll-schedules',
     templateUrl: './app/modules/side-scroll-schedules/side-scroll-schedules.module.html',
-    directives: [],
+    directives: [ScheduleBox],
     providers: []
 })
 
