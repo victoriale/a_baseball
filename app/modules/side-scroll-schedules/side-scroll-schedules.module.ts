dadeb0c8ece0d77bdacf559ef30457525fadc357
LarryHPham <larrypham1991@yahoo.com>
2016-07-14 10:05:30 -0500

    seperated side scroll

--- app/modules/side-scroll-schedules/side-scroll-schedules.module.ts
+++ app/modules/side-scroll-schedules/side-scroll-schedules.module.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {ScheduleBox} from '../../components/schedule-box/schedule-box.component'
+import {SideScroll} from '../../components/carousels/side-scroll/side-scroll.component'
 
 @Component({
     selector: 'side-scroll-schedules',
     templateUrl: './app/modules/side-scroll-schedules/side-scroll-schedules.module.html',
-    directives: [ScheduleBox],
+    directives: [ScheduleBox,SideScroll],
     providers: []
 })
 
