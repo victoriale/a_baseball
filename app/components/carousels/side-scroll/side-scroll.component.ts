3d054a804171433d585c7d57649055ccf2c55848
LarryHPham <larrypham1991@yahoo.com>
2016-07-18 18:55:27 -0500

    added owl carousel to side scroll but with issues

--- app/components/carousels/side-scroll/side-scroll.component.ts
+++ app/components/carousels/side-scroll/side-scroll.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
 
+declare var jQuery:any
+;
 @Component({
     selector: 'side-scroll',
     templateUrl: './app/components/carousels/side-scroll/side-scroll.component.html',
@@ -7,6 +9,27 @@ import {Component, OnInit, Input} from '@angular/core';
     providers: [],
 })
 
-export class SideScroll{
+export class SideScroll implements OnInit{
+
+  ngOnInit(){
+    jQuery(".owl-carousel").owlCarousel({
+      items:5,
+      loop:true,
+      dots:false,
+      nav:false,
+      navText:false
+    });
+  }
+
+  left() {
+    var owl = jQuery('#ss_owl');
+    owl.owlCarousel();
+    owl.trigger('prev.owl.carousel');
+  }
+  right() {
+    var owl = jQuery('#ss_owl');
+    owl.owlCarousel();
+    owl.trigger('next.owl.carousel');
+  }
 
 }
