56c1797af4d0376a6119ea0b66338a30b60b9c4f
LarryHPham <larrypham1991@yahoo.com>
2016-07-21 09:57:24 -0500

    added owl carousel through angular 2

--- app/components/carousels/side-scroll/side-scroll.component.ts
+++ app/components/carousels/side-scroll/side-scroll.component.ts
@@ -1,5 +1,5 @@
-import {Component, AfterContentChecked, Input, Output, EventEmitter} from '@angular/core';
-
+import {Component, AfterContentChecked, Input, Output, EventEmitter, ElementRef} from '@angular/core';
+import 'owl-carousel';
 declare var jQuery:any;
 
 @Component({
@@ -8,32 +8,48 @@ declare var jQuery:any;
     outputs: ['carouselCount']
 })
 
-export class SideScroll implements AfterContentChecked{
+export class SideScroll{
+  @Input() options: Object;
   @Input() maxLength:any;
   @Input() current:any;
   public carouselCount = new EventEmitter();
   public count = 0;
-  constructor(){
+
+  owlElement: any;
+  defaultOptions: Object = {};
+
+  constructor(private _el: ElementRef){
   }
 
-  ngAfterContentChecked(){
-    var self = this;
-    var owl = jQuery('.ss_owl');
-    owl.owlCarousel({
-      items:7,
-      loop:false,
-      dots:false,
-      nav:false,
-      navText:false,
-    });
-    owl.on('changed.owl.carousel', function(event) {
-        var currentItem = event.item.index;
-        if(self.count != currentItem){
-          self.carouselCount.next(currentItem);
-        }
-        self.count = currentItem;
-    })
+  ngAfterViewInit() {
+    for (var key in this.options) {
+      this.defaultOptions[key] = this.options[key];
+    }
+    this.owlElement = jQuery(this._el.nativeElement).owlCarousel(this.defaultOptions);
   }
+
+  ngOnDestroy() {
+    this.owlElement.data('owlCarousel').destroy();
+    this.owlElement = null;
+  }
+  // ngAfterContentChecked(){
+  //   var self = this;
+  //   var owl = jQuery('.ss_owl');
+  //   owl.owlCarousel({
+  //     items:7,
+  //     loop:false,
+  //     dots:false,
+  //     nav:false,
+  //     navText:false,
+  //   });
+  //   owl.on('changed.owl.carousel', function(event) {
+  //       var currentItem = event.item.index;
+  //       if(self.count != currentItem){
+  //         self.carouselCount.next(currentItem);
+  //       }
+  //       self.count = currentItem;
+  //   })
+  // }
 
   left() {
     var owl = jQuery('.ss_owl');
