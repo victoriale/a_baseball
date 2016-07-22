1d0b9950c58f5929dc5715b883e72570f296c971
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2016-07-21 21:23:49 -0500

    video tiles

--- app/components/video-stacktop/video-stacktop.component.ts
+++ app/components/video-stacktop/video-stacktop.component.ts
@@ -1,12 +1,14 @@
 import {Component,OnInit} from '@angular/core';
 import {DeepDiveService} from '../../services/deep-dive.service';
+import {SanitizeHtml} from "../../pipes/safe.pipe";
 
 
 @Component({
   selector: 'video-stacktop-component',
   templateUrl: './app/components/video-stacktop/video-stacktop.component.html',
   directives: [],
-  providers: [DeepDiveService]
+  providers: [DeepDiveService],
+  pipes: [SanitizeHtml]
 
 })
 
@@ -15,13 +17,12 @@ export class VideoStacktopComponent{
   constructor(
     private _deepdiveservice:DeepDiveService
     ){
-      this.getDeepDiveVideoBatch(1, 2);
+      this.getDeepDiveVideoBatch(2, 1);
     }
     private getDeepDiveVideoBatch(numItems, startNum){
       this._deepdiveservice.getDeepDiveVideoBatchService(numItems, startNum).subscribe(
         data => {
           this.articleData = data.data;
-          console.log(this.articleData);
         }
       )
     }
