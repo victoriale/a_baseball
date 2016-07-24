a939f89bb7cbdaf7f1734e3ee8361f90ca04e5ee
LarryHPham <larrypham1991@yahoo.com>
2016-07-23 20:49:53 -0500

    responsive fix

--- app/components/responsive-widget/responsive-widget.component.ts
+++ app/components/responsive-widget/responsive-widget.component.ts
@@ -12,13 +12,22 @@ export class ResponsiveWidget implements OnInit {
   @Input() displayAtRes: string;
 
   windowWidth: number = 10;
+  widgetMed:boolean=false;
+  widgetSml:boolean=false;
+
   ngOnInit() {
     this.displayAtRes = "_" + this.displayAtRes + "only"
     window.onresize = this.onWindowLoadOrResize;
-    this.onWindowLoadOrResize();
   }
-  private onWindowLoadOrResize() {
-    this.windowWidth = window.innerWidth;
+  private onWindowLoadOrResize(event) {
+    var windowWidth = event.target.outerWidth;
+    console.log(windowWidth);
+    console.log(event);
+    if(windowWidth <= 640){
+      this.widgetSml = true;
+    }else if(windowWidth <= 1024 && windowWidth > 640){
+      this.widgetMed = true;
+    }
+    console.log(this.widgetMed, this.widgetSml);
   }
-
 }
