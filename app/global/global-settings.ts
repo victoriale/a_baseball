b807cd91161b5361fdf85e03f84cfcdaa3bc4c0c
Jonathan Van Winkle <jonathan.vanwinkle@sntmedia.com>
2016-07-26 05:15:50 -0500

    removed testing code for local parnter pages

--- app/global/global-settings.ts
+++ app/global/global-settings.ts
@@ -108,8 +108,8 @@ export class GlobalSettings {
       var isHome = false;
       var hide = false;
       var hostname = window.location.hostname;
-      //var partnerPage = /myhomerunzone/.test(hostname);
-      var partnerPage = /localhost/.test(hostname);
+      var partnerPage = /myhomerunzone/.test(hostname);
+      //var partnerPage = /localhost/.test(hostname);
       var name = window.location.pathname.split('/')[1];
       //console.log("GlobalSettings:", 'partnerPage =>', partnerPage, 'name =>', name);
 
