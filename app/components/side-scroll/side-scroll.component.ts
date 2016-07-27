2dd4ee1060f3657cd8e82b5cf0bab5345dcd586f
LarryHPham <larrypham1991@yahoo.com>
2016-07-27 15:20:55 -0500

    added new functions for carousel to clone items for display

--- app/components/side-scroll/side-scroll.component.ts
+++ app/components/side-scroll/side-scroll.component.ts
@@ -1,4 +1,4 @@
-import {Component, AfterContentChecked, Input, Output, EventEmitter} from '@angular/core';
+import {Component, AfterContentChecked, Input, Output, EventEmitter, ElementRef} from '@angular/core';
 import {SanitizeStyle, SanitizeHtml} from '../../pipes/safe.pipe';
 import {ScheduleBox} from '../schedule-box/schedule-box.component'
 import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
@@ -31,49 +31,96 @@ export class SideScroll{
 
   private transition:any = null;
 
-  @Input('total-items') totalItems:number;
+  @Input('show-item-num') totalItems:number;
   @Input('loop') loop:boolean;
-
+  @Input('fade') fade:boolean;
+  @Input('arrow')
   private startIndex:number = 0;
   private endIndex:number = 1;
   private displayedItems:any;
 
-  constructor(){
+  constructor(private _elRef: ElementRef){
 
   }
   ngOnInit(){
+
+    //delete below when done testing
+    this.data.length = 2;
     console.log(this.data);
-    this.startIndex = 0;
-    this.endIndex =this.data.length - 1;
+
+    if(this.totalItems == null){// if no input comes from totalItems then default to showing 1 item in carousel
+      this.totalItems = 1;
+    }
+    this.startIndex = 0;//on initial load of component start index at 1
+    this.endIndex = (this.startIndex + (this.totalItems-1)) % this.data.length; //the ending index needs to be the start of the index
     this.generateArray();
+    if(this.maxLength == null){
+      this.maxLength = this.data.length;
+    }
+
   }
 
-  ngOnChanges(){
+  ngAfterViewInit(){
+    this.itemSize = this._elRef.nativeElement.getElementsByClassName('carousel_scoll-container')[0].offsetWidth;
   }
 
   generateArray(){
     var originalData = this.data;
-    var total = 7;
+    var total = this.totalItems;
     this.displayedItems = [];
-    this.currentScroll = 0;
+    // this.currentScroll = 0;
     //if loops from input is true then the hidden item needs to be the last item of the array + the prev item before that
-    for(var i = 0; i < 7; i++){
-      console.log('i=',i,', start=',this.startIndex);
+    console.log('total',total);
+    for(var i = 0; i < total; i++){
       this.displayedItems.push({
         id:i,
         data:originalData[this.startIndex]
       });
-      console.log(originalData[this.startIndex].date);
-      this.startIndex++;
-      this.endIndex++;
-      if( this.startIndex > originalData.length - 1){
-        this.startIndex = 0;
-      }
-      if( this.endIndex > originalData.length - 1){
-        this.endIndex = 0;
-      }
+      this.startIndex = (this.startIndex + 1) % originalData.length;
+      this.endIndex = (this.endIndex + 1) % originalData.length;
+      // if( this.startIndex > originalData.length - 1){
+      //   this.startIndex = 0;
+      // }
+      // if( this.endIndex > originalData.length - 1){
+      //   this.endIndex = 0;
+      // }
     }
-    console.log('Displaying',this.displayedItems);
+    console.log(this.displayedItems);
+    for(var o = 1; o < 3; o++){
+      //grab the cloned indexes in the array;
+      console.log(this.startIndex, this.endIndex);
+      var cloneBefore = (this.startIndex - o);
+      var cloneAfter = (this.endIndex + o) % originalData.length;
+      console.log('indexBefore:', cloneBefore, 'indexAfter:', cloneAfter);
+      //sanity check in case there is only 2 items
+      if(originalData.length <= 2){
+        cloneAfter = cloneAfter > 1 ? cloneAfter : 0;
+        cloneBefore = cloneBefore < 0 ? cloneBefore : 0;
+      }
+
+      //will get the index of the two previos items but will go back to the top index if it goes below 0
+      if(cloneBefore >= 0){
+        cloneBefore = cloneBefore % originalData.length;
+      }else{
+        cloneBefore = originalData.length + cloneBefore;
+      }
+
+      //unshift will push the cloned items infront of the array
+      this.displayedItems.unshift({
+        id:cloneBefore,
+        data:originalData[cloneBefore]
+      })
+      //push will push the cloned items at the end of the array
+      this.displayedItems.push({
+        id:cloneAfter,
+        data:originalData[cloneAfter]
+      })
+      console.log(cloneBefore,this.displayedItems, cloneAfter);
+    }
+  }
+
+  checkRightShift(){
+
   }
 
   left(event) {
@@ -115,6 +162,7 @@ export class SideScroll{
         this.currentScroll = 0;
       }
       this.rightText = this.currentScroll+'px';
+      console.log(this.rightText);
     }
   }
 
