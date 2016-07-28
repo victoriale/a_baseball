5af7191241c74378d86b75df9dea66e943091803
Alex Varenhorst <alexvarenhorst@gmail.com>
2016-07-28 09:23:26 -0500

    carousel responsivess at 320px

--- app/services/deep-dive.service.ts
+++ app/services/deep-dive.service.ts
@@ -193,7 +193,7 @@ export class DeepDiveService {
           image_url: GlobalSettings.getImageUrl(val['imagePath']),
           title:  "<span> Today's News </span>" + val['title'],
           keyword: val['keyword'],
-          teaser: val['teaser'].substr(0,250).replace('_',': ').replace(/<p[^>]*>/g, "") + "...",
+          teaser: val['teaser'].substr(0,200).replace('_',': ').replace(/<p[^>]*>/g, "") + "...",
           id:val['id'],
           articlelink: MLBGlobalFunctions.formatSynRoute('story', val.id),
           // style: val['style']
