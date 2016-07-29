507db9813e40431808e80e2b99a68fe4b8d3924e
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2016-07-29 11:33:13 -0500

    sticky blue bar

--- app/modules/widget/widget-carousel.module.ts
+++ app/modules/widget/widget-carousel.module.ts
@@ -76,11 +76,13 @@ export class WidgetCarouselModule {
           carouselTop = 0;
         }
         //this.headerHeight = carouselTop + padding + maxScroll + this.sidekickHeight + 'px';
-        if ((document.getElementById('partner') != null && maxScroll == 0) || (document.getElementById('partner') == null && carouselTop == 0)) {
-          jQuery("#widget").addClass("widget-top");
+        if ((document.getElementById('partner') != null && maxScroll <= 50) || (document.getElementById('partner') == null && carouselTop <= 50)) {
+          jQuery("#widget").addClass("widget-top-ddp");
+          jQuery("#deep-dive-blueBar").addClass("deep-dive-blueBar-top");
         }
         else {
-          jQuery("#widget").removeClass("widget-top");
+          jQuery("#widget").removeClass("widget-top-ddp");
+          jQuery("#deep-dive-blueBar").removeClass("deep-dive-blueBar-top");
         }
         var $widget = jQuery("#widget");
         var $pageWrapper = jQuery(".deep-dive-container2a");
