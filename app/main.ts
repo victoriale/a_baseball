392ed1511362016d9429b3175795ad7a18b57815
Jonathan Van Winkle <jonathan.vanwinkle@sntmedia.com>
2016-08-19 09:22:34 -0500

    only fires enableProdMode and google analytics if in prod

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,7 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {AppDomain} from './app-domain/app.domain';
 import {GlobalFunctions} from './global/global-functions';
+import {GlobalSettings} from './global/global-settings';
 import {MLBGlobalFunctions} from './global/mlb-global-functions';
 import {SearchService} from './services/search.service';
 import {DraftHistoryService, MLBDraftHistoryService} from './services/draft-history.service'; //testing a proof of concept
@@ -11,10 +12,13 @@ import {provide} from "@angular/core";
 // Needed for http map on observables
 import 'rxjs/add/operator/map';
 import {HTTP_PROVIDERS} from "@angular/http";
+import {enableProdMode} from '@angular/core';
 
-  // import {enableProdMode} from '@angular/core';
-  // enable production mode and thus disable debugging information
-  // enableProdMode();
+
+// enable production mode and thus disable debugging information
+if(GlobalSettings.isProd()) {
+  enableProdMode();
+}
 
 bootstrap(AppDomain, [
     ROUTER_PROVIDERS,
